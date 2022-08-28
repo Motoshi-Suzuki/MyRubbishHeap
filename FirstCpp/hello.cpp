5bf0ad63d127aaa6a4f88b6a854768065ec30c66
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2022-08-28 14:02:24 +0900

    first cpp

--- FirstCpp/hello.cpp
+++ FirstCpp/hello.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
- 
-int main()
-{
-    cout << "Hello World" << endl;
+
+int main() {
+    int x = 10;
+    int y = 20;
+    cout << "x = " << x << endl 
+              << "y = " << y;
     return 0;
 }
